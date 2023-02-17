d206be661ad6d3305e6833110d0c3127afbc33a6
n-shirasaki <n-shirasaki@cladminnoMacBook-Pro.local>
2023-02-17 14:25:33 +0900

    FedCMのサンプル実装

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 FedCM の動作確認用
 =====
+
+client
+```
+cd client/
+python -m http.server
+```
+
+server  
+```
+cd server/
+pip install -r requirements.txt
+python main.py
+```
--- server/main.py
+++ server/main.py
@@ -19,11 +19,72 @@ app.add_middleware(
 async def health():
     return {"text": "OK"}
 
+@app.get("/.well-known/web-identity")
+async def wellKnown():
+    return {
+        "provider_urls": ["http://localhost:8080/config.json"],
+    }
+
+@app.get("/config.json")
+async def config():
+    return {
+        "accounts_endpoint": "/accounts",
+        "client_metadata_endpoint": "/client_metadata",
+        "id_assertion_endpoint": "/assertion",
+        "branding": {
+            "background_color": "green",
+            "color": "0xFFEEAA",
+            "icons": [{
+                "url": "http://localhost:8080/icon.ico",
+                "size": 25
+            }]
+        }
+    }
+
+@app.get("/accounts")
+async def accounts():
+    return {
+        "accounts": [{
+            "id": "1234",
+            "given_name": "John",
+            "name": "John Doe",
+            "email": "john_doe@idp.example",
+            "picture": "http://localhost:8080/profile/123",
+            "approved_clients": ["123", "456", "789"],
+        }, {
+            "id": "5678",
+            "given_name": "Johnny",
+            "name": "Johnny",
+            "email": "johnny@idp.example",
+            "picture": "http://localhost:8080/profile/456",
+            "approved_clients": ["abc", "def", "ghi"],
+        }]
+    }
+
+@app.get("/client_metadata")
+async def clientMetadata():
+    return {
+        "privacy_policy_url": "http://localhost:8080/privacy_policy.html",
+        "terms_of_service_url": "http://localhost:8080/terms_of_service.html",
+    }
+
+@app.post("/assertion")
+async def assertion():
+    return {
+        "token": "12345",
+    }
+
+@app.get("/profile/{id}")
+async def profile(id):
+    print("ID: {}".format(id))
+    return {
+        "id": id,
+    }
+
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
         reload=True,
         port=8080,
-        debug=True,
     )
